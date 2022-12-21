61f460652ef1dc0d6daa11ba61425ec9087f16cd
ketchupbear <ryan@sendwithus.com>
2022-12-20 18:48:22 -0800

    initial upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130670275-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-130670275-1');
+</script>
+
+	<title>Ryan Stinson - Victoria, BC based Product Manager and Designer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable0">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">  
+    <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">  
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+<div class="container">
+	<div class="portrait"></div>
+	<div class="info">
+		<H1>Hi, I'm Ryan.</H1>
+		<p>I'm currently a Product Manager at <a href="https://dyspatch.io/" target="_blank">Dyspatch</a> in Victoria, BC where I specialize in discovery, strategy, and assisting in building user-focused solutions through creative problem solving with my team.</p>
+
+		<p>Some names that you may recognize that I've worked with are City of Saskatoon, Leo Insurance, Variety BC Children's Charity, Triple O's, and Saskatchewan Liquor and Gaming.</p>
+		<a href="mailto:ryan.stinson@gmail.com?subject=Hello from RyanStinson.ca" class="button">Get in Touch</a>
+		<div class="social">
+			<ul>
+				<li><a href="http://twitter.com/ryanstinson" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></a></i></li>
+				<li><a href="https://www.linkedin.com/in/rstinson/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+				<li><a href="https://medium.com/@ryanstinson" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a></li>
+
+			</ul>
+		</div>
+	</div>
+</div>
+</body>
+</html>
